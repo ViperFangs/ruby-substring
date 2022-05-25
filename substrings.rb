66296213177c14b15707fc6d5c6e3e7b02de817a
Aarya <amathrej@asu.edu>
2022-05-25 05:12:31 -0700

    Change if condition inside reduce method

--- substrings.rb
+++ substrings.rb
@@ -1,15 +1,10 @@
 def substrings(word, dictionary)
 
   return_hash = dictionary.reduce(Hash.new(0)) do |result, current_word|
-    if word.include?(current_word) 
-      result[current_word] += 1
-    end
-
+    result[current_word] += 1 if word.include?(current_word)
     result
-    
   end
   
   puts return_hash
-  return return_hash
 
 end
